03b5c295e487edb97fb044b65c1b8132abb5c0ac
remorses <beats.by.morse@gmail.com>
2023-04-10 13:50:07 +0200

    use mansory layout

--- website/src/components/app.tsx
+++ website/src/components/app.tsx
@@ -1,4 +1,6 @@
 import classNames from 'classnames'
+import Masonry from 'react-masonry-css'
+
 import { useRef, useEffect } from 'react'
 
 // import "fabric-history"
@@ -125,23 +127,86 @@ export function App({}) {
         <div className='w-screen h-screen flex flex-col dark'>
             <div className='relative bg-gray-600 h-full flex'>
                 <LeftPane />
+                <style jsx global>{`
+                    .my-masonry-grid {
+                        display: flex;
+                        margin-left: -30px; /* gutter size offset */
+                        width: 100%;
+                    }
+                    .my-masonry-grid_column {
+                        padding-left: 30px; /* gutter size */
+                        background-clip: padding-box;
+                    }
 
+                    /* Style your items */
+                    .my-masonry-grid_column > div {
+                        /* change div to reference your elements you put in <Masonry> */
+                        background: grey;
+                        margin-bottom: 30px;
+                    }
+                `}</style>
+                <Masonry
+                    breakpointCols={3}
+                    className='my-masonry-grid overflow-y-auto p-[40px]'
+                    columnClassName='my-masonry-grid_column'
+                >
+                    {!generations?.length &&
+                        Array.from({ length: 12 }).map((_, i) => (
+                            <div
+                                style={{
+                                    aspectRatio: i >= 4 ? '1/1' : '16/9',
+                                    // if aspect ratio is 1/2 then it takes 2 rows
+                                    // if aspect ratio is 1/1 then it takes 1 row
+                                    // gridRow: i == 2 ? 'span 2' : 'span 1',
+                                    // gridTemplateRows:
+                                    //     i == 2 ? '1fr 1fr' : '1fr',
+                                }}
+                                key={i}
+                                className='flex text-2xl text-white flex-col items-center justify-center rounded-md bg-gray-700 '
+                            >
+                                {i}
+                            </div>
+                        ))}
+                </Masonry>
+{/* 
                 <div
                     className={classNames(
-                        'flex-1 relative p-[40px] grid gap-[40px] grid-cols-1 lg:grid-cols-4 w-full h-full min-w-0 bg-gray-600',
+                        'flex-1 relative items-start gap-[40px]  space-y-12 place-content-start p-[40px] columns-4 w-full h-full min-w-0 bg-gray-600',
                     )}
                 >
                     {!generations?.length &&
-                        Array.from({ length: 4 }).map((_, i) => (
+                        Array.from({ length: 8 }).map((_, i) => (
                             <div
                                 style={{
                                     aspectRatio: '1/1',
+                                    // if aspect ratio is 1/2 then it takes 2 rows
+                                    // if aspect ratio is 1/1 then it takes 1 row
+                                    // gridRow: i == 2 ? 'span 2' : 'span 1',
+                                    // gridTemplateRows:
+                                    //     i == 2 ? '1fr 1fr' : '1fr',
                                 }}
-                                className='rounded-md bg-gray-700 '
-                            ></div>
+                                key={i}
+                                className='flex text-2xl text-white flex-col items-center justify-center rounded-md bg-gray-700 '
+                            >
+                                {i}
+                            </div>
                         ))}
-                </div>
+                </div> */}
             </div>
         </div>
     )
+}
+
+function GenerationImage({ aspectRatio }) {}
+
+function aspectRatioToStyle(aspectRatio: number) {
+    // aspectRatio is width / height
+    if (!aspectRatio) {
+        return
+    }
+    let above = aspectRatio * 100
+    let below = 100
+    let gcd = getGCD(above, below)
+    above /= gcd
+    below /= gcd
 }
