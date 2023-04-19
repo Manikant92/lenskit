3a79e25fc865fe2d3e5981595fcb1a99d0ec63e6
remorses <beats.by.morse@gmail.com>
2023-04-19 12:07:41 +0200

    website/src/pages/_app:

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -69,17 +69,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
                     }
                 }}
             />
-            <Script
-                async
-                src='https://www.googletagmanager.com/gtag/js?id=G-7NTWFXR4X3'
-            ></Script>
-            <Script id='gtag'>
-                {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7NTWFXR4X3');`}
-            </Script>
-            <Script
-                id='crisp'
-                type='text/javascript'
-            >{`window.$crisp=[];window.CRISP_WEBSITE_ID="794809b8-5948-4269-a3ba-33ec690dabd7";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}</Script>
+            
+
             <BeskarProvider>
                 <ThemeProvider
                     defaultTheme='dark'
